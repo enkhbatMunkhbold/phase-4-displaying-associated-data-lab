4dc452253eaceb245f5da04e9a84fa7ca5516e92
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-06-03 21:18:40 -0700

    commit tree 14caf8d6733f3baeab327c1446db271766436bd1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+  def index
+    items = Item.all
+    render json: items, except: [:user_id, :created_at, :updated_at], include: :user
+  end
+
 end
